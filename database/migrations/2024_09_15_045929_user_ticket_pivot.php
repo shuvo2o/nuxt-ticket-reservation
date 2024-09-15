d8e24a71936fef12d243f138a8dc4f18cfc4050c
prismtechbd3 <prismtechb@gmail.com>
2024-09-15 11:16:35 +0600

    nuxt ticket reservation schema and migration done

---
+++ database/migrations/2024_09_15_045929_user_ticket_pivot.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_user', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUlid('ticket_id')->constrained()->cascadeOnDelete();
+            $table->foreignUlid('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_user');
+    }
+};
